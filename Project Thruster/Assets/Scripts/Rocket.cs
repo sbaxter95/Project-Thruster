14845f70b4706da47a4222c192154537d950ec2b
Sophie Baxter <sbaxter95@outlook.com>
2018-03-03 14:05:09 +0000

    Started rocket script

--- Project Thruster/Assets/Scripts/Rocket.cs
+++ Project Thruster/Assets/Scripts/Rocket.cs
@@ -11,6 +11,22 @@ public class Rocket : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        ProcessInput();
 	}
+
+    private void ProcessInput()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            print("Space pressed");
+        }
+        else if(Input.GetKey(KeyCode.A))
+        {
+            print("Rotating left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("Rotating right");
+        }
+    }
 }
