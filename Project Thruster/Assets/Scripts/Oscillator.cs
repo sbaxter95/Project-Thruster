f36a638a637b48f8b1e00bddcbf1607951f87c18
Sophie Baxter <sbaxter95@outlook.com>
2018-03-31 11:31:25 +0100

    Added oscillation and made some game objects move

---
+++ Project Thruster/Assets/Scripts/Oscillator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour {
+
+    [SerializeField]
+    Vector3 movementVector = new Vector3(10f, 0f, 0f);
+
+    [SerializeField]
+    float period = 2f;
+
+    Vector3 startingPos;
+
+	// Use this for initialization
+	void Start () {
+        startingPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //set movement factor automatically
+        float cycles = Time.time / period; //grows continually from 0
+
+        const float tau = Mathf.PI * 2;
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        movementFactor = rawSinWave / 2f + 0.5f;
+
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPos + offset;
+	}
+}
